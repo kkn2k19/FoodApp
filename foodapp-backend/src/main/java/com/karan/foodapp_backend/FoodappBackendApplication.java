88154c403a697ed9cda680f8132ba1d7250fda96
kkn2k19 <karankumarnonia11@gmail.com>
2025-03-03 02:31:24 +0530

    Food App Backend part

---
+++ foodapp-backend/src/main/java/com/karan/foodapp_backend/FoodappBackendApplication.java
@@ -0,0 +1,17 @@
+package com.karan.foodapp_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication(scanBasePackages = "com.karan")
+@EnableJpaRepositories(basePackages = "com.karan")
+@EntityScan(basePackages = "com.karan")
+public class FoodappBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodappBackendApplication.class, args);
+	}
+
+}
