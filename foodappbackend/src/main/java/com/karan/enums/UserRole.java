e1858cb7fd072f1376a96f1439b095432447e6dc
Karan Kumar Nonia <karankumarnonia11@gmail.com>
2025-09-01 17:39:26 +0530

    enums for email as well as verificationTypes

---
+++ foodappbackend/src/main/java/com/karan/enums/UserRole.java
@@ -0,0 +1,10 @@
+package com.karan.enums;
+
+public enum UserRole {
+    ADMIN,
+    USER
+}
+
+// normal user - user role
+// admin for managing others like getting all active users data and can do
+// manage them
---
+++ foodappbackend/src/main/java/com/karan/enums/VerificationType.java
@@ -0,0 +1,6 @@
+package com.karan.enums;
+
+public enum VerificationType {
+    VERIFY, // verify type for email verification
+    RESET // reset will be for password reset
+}
