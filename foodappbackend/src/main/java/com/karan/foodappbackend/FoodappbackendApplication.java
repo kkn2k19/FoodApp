d23c996b00f6a836534f0e368eaa517c54507ea3
Karan Kumar Nonia <karankumarnonia11@gmail.com>
2025-09-01 17:47:36 +0530

    updated

--- foodappbackend/src/main/java/com/karan/foodappbackend/FoodappbackendApplication.java
+++ foodappbackend/src/main/java/com/karan/foodappbackend/FoodappbackendApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 @SpringBootApplication(scanBasePackages = "com.karan")
 @EnableJpaRepositories(basePackages = "com.karan")
 @EntityScan(basePackages = "com.karan")
+@EnableMethodSecurity
 public class FoodappbackendApplication {
 
 	public static void main(String[] args) {
