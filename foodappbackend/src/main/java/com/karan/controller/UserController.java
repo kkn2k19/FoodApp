18090cab607602bbe80966e998c66cb0c78e4682
Karan Kumar Nonia <karankumarnonia11@gmail.com>
2025-09-01 17:42:34 +0530

    updated user controller

---
+++ foodappbackend/src/main/java/com/karan/controller/UserController.java
@@ -0,0 +1,89 @@
+package com.karan.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.karan.dto.EmailVerifyRequest;
+import com.karan.dto.ForgotPassRequest;
+import com.karan.dto.JWTResponse;
+import com.karan.dto.LoginRequest;
+import com.karan.dto.RegisterRequest;
+import com.karan.dto.ResendOtpRequest;
+import com.karan.dto.ResetPassRequest;
+import com.karan.service.EmailService;
+import com.karan.service.UserAuthService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final EmailService emailService;
+    private final UserAuthService userAuthService;
+
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@RequestBody RegisterRequest request) {
+        try {
+            String msg = userAuthService.register(request);
+            return ResponseEntity.ok().body(msg);
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody LoginRequest request) {
+        try {
+            JWTResponse response = userAuthService.login(request);
+            return ResponseEntity.ok().body(response);
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PostMapping("/verify-email")
+    public ResponseEntity<?> verifyEmail(@RequestBody EmailVerifyRequest request) {
+        try {
+            String msg = userAuthService.verifyEmail(request);
+            return ResponseEntity.ok().body(msg);
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PostMapping("/forgot-password")
+    public ResponseEntity<?> forgotPassword(@RequestBody ForgotPassRequest request) {
+        try {
+            String msg = userAuthService.forgotPassword(request);
+            return ResponseEntity.ok().body(msg);
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PostMapping("/reset-password")
+    public ResponseEntity<?> resetPassword(@RequestBody ResetPassRequest request) {
+        try {
+            String msg = userAuthService.resetPassword(request);
+            return ResponseEntity.ok().body(msg);
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PostMapping("/resend-otp")
+    public ResponseEntity<?> resendOtp(@RequestBody ResendOtpRequest request) {
+        try {
+            String msg = "OTP resent successfully.";
+            emailService.resendOtp(request.getEmail(), request.getType());
+            return ResponseEntity.ok().body(msg);
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+}
