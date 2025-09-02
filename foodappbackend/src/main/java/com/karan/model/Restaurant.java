2a2172d9e6c5d3169307a66a77cf9fd4cd07b440
Karan Kumar Nonia <karankumarnonia11@gmail.com>
2025-09-02 23:03:43 +0530

    Restaurant Model updated with OnetoMany with FoodItems

--- foodappbackend/src/main/java/com/karan/model/Restaurant.java
+++ foodappbackend/src/main/java/com/karan/model/Restaurant.java
@@ -1,11 +1,20 @@
 package com.karan.model;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -25,12 +34,14 @@ public class Restaurant {
     private Long id;
 
     @Column(length = 25, nullable = false)
+    @NotBlank
     private String name;
 
     @Column(length = 30, nullable = false)
     private String email;
 
     @Column(length = 10, nullable = false)
+    @Pattern(regexp = "\\d{10}", message = "Phone must be 10 digits")
     private String phone;
 
     private boolean active = true; // status - open/closed
@@ -46,5 +57,11 @@ public class Restaurant {
     private String state;
 
     @Column(nullable = false, length = 6)
+    @Pattern(regexp = "\\d{6}", message = "Pincode must be 6 digits")
     private String pincode;
+
+    // one restaurant can have many food items
+    @OneToMany(mappedBy = "restaurant", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    @JsonIgnore // <-- prevent recursive serialization
+    private List<FoodItem> foodItems;
 }
