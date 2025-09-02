95566174b5d729acf9e0e4a8c437a2e6ba46437b
Karan Kumar Nonia <karankumarnonia11@gmail.com>
2025-09-02 23:32:05 +0530

    added order model

--- foodappbackend/src/main/java/com/karan/model/Order.java
+++ foodappbackend/src/main/java/com/karan/model/Order.java
@@ -1,88 +1,53 @@
 package com.karan.model;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.hibernate.annotations.CreationTimestamp;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+// Order - represents a whole order placed by a user --
+// orderItem -represents a single food item inside an order
 
 @Entity
-@Table(name = "order_dtls")
+@Table(name = "orders")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer oid;
-    @CreationTimestamp
-    @Temporal(TemporalType.DATE)
-    private Date odt;
-    @Column(length = 10, nullable = false)
-    private String fid;
-    private Integer oqty;
-    @Column(length = 10, nullable = false)
-    private String uname;
+    private Long id;
 
-    public Order() {
-        super();
-    }
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
 
-    public Order(Integer oid, Date odt, String fid, Integer oqty, String uname) {
-        this.oid = oid;
-        this.odt = odt;
-        this.fid = fid;
-        this.oqty = oqty;
-        this.uname = uname;
-    }
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+    private List<OrderItem> items = new ArrayList<>();
 
-    public Integer getOid() {
-        return oid;
-    }
+    private LocalDateTime orderDate;
 
-    public void setOid(Integer oid) {
-        this.oid = oid;
-    }
-
-    public Date getOdt() {
-        return odt;
-    }
-
-    public void setOdt(Date odt) {
-        this.odt = odt;
-    }
-
-    public String getFid() {
-        return fid;
-    }
-
-    public void setFid(String fid) {
-        this.fid = fid;
-    }
-
-    public Integer getOqty() {
-        return oqty;
-    }
-
-    public void setOqty(Integer oqty) {
-        this.oqty = oqty;
-    }
-
-    public String getUname() {
-        return uname;
-    }
-
-    public void setUname(String uname) {
-        this.uname = uname;
-    }
-
-    @Override
-    public String toString() {
-        return "Order [oid=" + oid + ", odt=" + odt + ", fid=" + fid + ", oqty=" + oqty + ", uname=" + uname + "]";
-    }
-
+    private String status; // PENDING, CONFIRMED, OUT_FOR_DELIVERY, DELIVERED, CANCELLED
 }
