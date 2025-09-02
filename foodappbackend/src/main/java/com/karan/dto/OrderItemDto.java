234fd482a44bafb9163ba8fa774df8995b87a9fd
Karan Kumar Nonia <karankumarnonia11@gmail.com>
2025-09-02 23:33:52 +0530

    dto - single order dto class

---
+++ foodappbackend/src/main/java/com/karan/dto/OrderItemDto.java
@@ -0,0 +1,14 @@
+package com.karan.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderItemDto {
+    private String foodName;
+    private int quantity;
+    private double price;
+}
