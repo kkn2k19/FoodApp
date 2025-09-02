e2bff7a21bf5d0ba6af76852bcdacca61d6b68b6
Karan Kumar Nonia <karankumarnonia11@gmail.com>
2025-09-02 23:36:10 +0530

    service interface order

--- foodappbackend/src/main/java/com/karan/service/OrderService.java
+++ foodappbackend/src/main/java/com/karan/service/OrderService.java
@@ -2,25 +2,22 @@ package com.karan.service;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import org.springframework.security.core.Authentication;
 
+import com.karan.dto.OrderDto;
 import com.karan.model.Order;
-import com.karan.repository.OrderRepository;
+import com.karan.model.User;
 
-@Service
-public class OrderService {
-    @Autowired
-    private OrderRepository orepo;
+public interface OrderService {
+    String placeOrder(User user);
 
-    // Add ORDER
-    public void addOrder(Order o) {
-        orepo.save(o);
-    }
+    List<OrderDto> getOrderForUser(User user);
 
-    // Get ORDERS / Billings (mixture so no datatype)
-    @SuppressWarnings("rawtypes") // it will supress warnings of List datatype not specified
-    public List billing() {
-        return orepo.billing();
-    }
+    OrderDto getOrderById(Long id, Authentication auth);
+
+    String updateOrderStatus(Long id, String status);
+
+    String cancelOrder(Long id, Authentication auth);
+
+    List<OrderDto> getAllOrders();
 }
