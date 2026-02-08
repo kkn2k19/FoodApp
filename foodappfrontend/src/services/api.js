2bd9569ebd15e71f9c5d0c0fd3cacebac921c09f
Karan Kumar Nonia <karankumarnonia11@gmail.com>
2026-02-08 22:02:52 +0530

    .

--- foodappfrontend/src/services/api.js
+++ foodappfrontend/src/services/api.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:1111",
+    baseURL: process.env.REACT_APP_API_URL,
 });
 
-api.interceptors.request.use((config)=>{
+api.interceptors.request.use((config) => {
     const token = localStorage.getItem("token");
-    if(token) {
+    if (token) {
         config.headers.Authorization = `Bearer ${token}`;
     }
     return config;
